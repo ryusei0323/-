3bf961e975b25f5703239a1b7d8ac5c08f4de58e
vagrant <vagrant@localhost.localdomain>
2020-08-03 08:08:21 +0000

    応用課題finish

--- Gemfile
+++ Gemfile
@@ -66,3 +66,7 @@ gem 'devise'
 gem 'refile', require: 'refile/rails', github: 'manfe/refile'
 gem 'refile-mini_magick'
 gem 'jquery-rails'
+#bootstrap追加
+gem 'bootstrap-sass', '~> 3.3.6'
+gem 'jquery-rails'
+#ここまで追加
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+##ここに移動
+  devise_for :users
+
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
-  devise_for :users
+#  <!-- devise_for :users移動 -->
   root 'home#top'
   get 'home/about'
 end
