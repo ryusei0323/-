9eb6902da92dd2eebf63db404c1bc6843e40d6d0
vagrant <vagrant@localhost.localdomain>
2020-08-06 09:40:36 +0000

    応用課題4 finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,30 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-   has_many :book_comments, dependent: :destroy
-   has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   attachment :profile_image, destroy: false
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, length: {maximum: 20, minimum: 2}
   validates :introduction, length: {maximum: 50}
+
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followings, through: :following_relationships
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
+
+  def following?(other_user)
+    following_relationships.find_by(following_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    following_relationships.create!(following_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    following_relationships.find_by(following_id: other_user.id).destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
 ##ここに移動
   devise_for :users
+  resources :users,only: [:show,:index,:edit,:update] do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
 
-  resources :users,only: [:show,:index,:edit,:update]
   resources :books, only: [:show, :index, :create, :destroy, :edit, :update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+
 #  <!-- devise_for :users移動 -->
   root 'home#top'
   get 'home/about'
+
 end
